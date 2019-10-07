ee61e570568d9439a31910d36240abc874d994c0
jvega190 <jose.vega@craftersoftware.com>
2019-10-07 11:38:08 -0600

    Updating to elasticsearch

--- scripts/rest/search.get.groovy
+++ scripts/rest/search.get.groovy
@@ -5,18 +5,33 @@ def search = params.searchValue
 def path = !params.path ? " ": params.path
 def recentVideosStatement = ''
 
-def videosHelper = new VideosHelper(searchService, siteItemService, start);
+def videosHelper = new VideosHelper(elasticsearch, urlTransformationService, start);
+
+logger.info("-------------------------------------------------------------------------------------\n")
+logger.info("{}", search)
+logger.info("-------------------------------------------------------------------------------------\n")
+logger.info("-------------------------------------------------------------------------------------\n")
+logger.info("{}", path)
+logger.info("-------------------------------------------------------------------------------------\n")
+
 
 if(search == '') {
-	recentVideosStatement = 'content-type:"/page/page-video"'
-} else if(path == '' || path == 'undefined'){
-	recentVideosStatement = 'content-type:"/page/page-video" AND (internal-name: *'+ search +'* OR tags.item.tagName: *'+search+'*)'
+	recentVideosStatement = "content-type:\"/page/page-video\""
+} else if(path == ' ' || path == 'undefined'){
+	// recentVideosStatement = 'content-type:"/page/page-video" AND (internal-name: *'+ search +'* OR tags.item.tagName: *'+search+'*)'
+	// recentVideosStatement = "content-type:\"/page/page-video\" AND title: \".*" + search + ".*\""
+	
+	recentVideosStatement = "title: \".*" + search + ".*\""
+	// recentVideosStatement = "title: \"" + search + "\""
 } else {
-	recentVideosStatement = 'content-type:"/page/page-video" AND categories.item.key:"'+ path +'" AND (internal-name: *'+ search +'* OR tags.item.tagName: *'+search+'*)'
+	// recentVideosStatement = 'content-type:"/page/page-video" AND categories.item.key:"'+ path +'" AND (internal-name: *'+ search +'* OR tags.item.tagName: *'+search+'*)'
+	// recentVideosStatement = "content-type:\"/page/page-video\" AND internal-name: \".*" + search + ".*\""
+	recentVideosStatement = "content-type:\"/page/page-video\""
 }
 
 if(search == '' && (path != 'undefined' || path != " ")) {
-	recentVideosStatement = 'content-type:"/page/page-video" AND categories.item.key:"'+ path +'"'
+	// recentVideosStatement = 'content-type:"/page/page-video" AND categories.item.key:"'+ path +'"'
+	recentVideosStatement = "content-type:\"/page/page-video\""
 }
 
 return videosHelper.getVideoList(recentVideosStatement)
--- static-assets/js/utils/videos_Utils.js
+++ static-assets/js/utils/videos_Utils.js
@@ -1,155 +1,157 @@
-function videoHandler(videoClass){
-    return $(videoClass).on('click', function () {
-        var formatedId= this.id.split('-').splice(1,this.id.length).join('-');
-        var video = document.getElementById('vid-'+formatedId)  
-        var playerIcon =  document.getElementById('player-'+formatedId)
-        var timeContainer = document.getElementById('time-'+formatedId)
-        
-        video.onseeking = function(){
-          video.controls = true;
-          video.paused = false;
-          playerIcon.style.visibility = "hidden";
-        }
-        
-        video.onseeked = function(){
-          video.controls = true;
-          video.paused = false;
-          playerIcon.style.visibility = "hidden";
-        }
-        
-        
-        video.onwebkitfullscreenchange = function(){
-        	if(!document.webkitFullscreenElement) {
-            	document.webkitExitFullscreen()
-                setTimeout(function(){
-                  $('.slider').slick('refresh')
-                  video.paused = false;
-                },500)
-            }  
-        }
-        
-       
+function videoHandler(videoClass) {
+  return $(videoClass).on('click', function () {
+    var formatedId = this.id.split('-').splice(1, this.id.length).join('-');
+    var video = document.getElementById('vid-' + formatedId)
+    var playerIcon = document.getElementById('player-' + formatedId)
+    var timeContainer = document.getElementById('time-' + formatedId)
 
-        video.onplaying = function() {            	
-          playerIcon.style.visibility = "hidden";
+    video.onseeking = function () {
+      video.controls = true;
+      video.paused = false;
+      playerIcon.style.visibility = "hidden";
+    }
+
+    video.onseeked = function () {
+      video.controls = true;
+      video.paused = false;
+      playerIcon.style.visibility = "hidden";
+    }
+
+
+    video.onwebkitfullscreenchange = function () {
+      if (!document.webkitFullscreenElement) {
+        document.webkitExitFullscreen()
+        setTimeout(function () {
+          $('.slider').slick('refresh')
           video.paused = false;
-          if(videoClass === '.carousel-player-container'){
-            timeContainer.style.display = "none";
-            $('.slick-arrow').css({display: 'none'})
-          }
-          }
-        
-        video.onpause = function() {
-          video.controls = true;
-          playerIcon.style.visibility = "visible";
-          $('.slick-arrow').css({display: 'block'})
-        }
-        
-        if(video.paused) {
-            video.play()
-            video.controls = true;
-            playerIcon.style.visibility = "hidden";
-        }
-    })
+        }, 500)
+      }
+    }
+
+
+
+    video.onplaying = function () {
+      playerIcon.style.visibility = "hidden";
+      video.paused = false;
+      if (videoClass === '.carousel-player-container') {
+        timeContainer.style.display = "none";
+        $('.slick-arrow').css({ display: 'none' })
+      }
+    }
+
+    video.onpause = function () {
+      video.controls = true;
+      playerIcon.style.visibility = "visible";
+      $('.slick-arrow').css({ display: 'block' })
+    }
+
+    if (video.paused) {
+      video.play()
+      video.controls = true;
+      playerIcon.style.visibility = "hidden";
+    }
+  })
 }
 
 function generateTags(tags) {
-    if(!tags || tags && tags.length === 0) return ["There are no tags"]
-  
-    return tags.map(function(tag){ 
-        if(!tag.tagName) return
-        
-        return tag.tagName
-    })
+  if (!tags || tags && tags.length === 0) return ["There are no tags"]
+
+  return tags.map(function (tag) {
+    if (!tag.tagName) return
+
+    return tag.tagName
+  })
 }
 
-function generateVideoUrl(url){
-    return url.split('/').splice(3).filter(function(item){
-    	return item != 'index.xml'
-    }).join('/')
+function generateVideoUrl(url) {
+  return url.split('/').splice(3).filter(function (item) {
+    return item != 'index.xml'
+  }).join('/')
 }
 
-function generateGridVideos(data){
-    var container = $("#gridContainer")
-    container.empty();
-    if(data.responseVideos.length === 0) {
-    	var content = document.getElementById("no-results-table-template")
-        if(!content) return
-        var source   = content.innerHTML;
-        var template = Handlebars.compile(source);
-        var context = {};
-		var html    = template(context);
-    	container.append(html)
-        return
-    }
-    const videos = data.responseVideos.map(function(video) {
-    	video.videoUrl = generateVideoUrl(video.src.storeUrl)
-        video.tags = generateTags(video.tags)
-        var content = document.getElementById("video-table-template")
-        if(!content) return 
-    	var source   = content.innerHTML;
-        var template = Handlebars.compile(source);
-        var context = video;
-		var html    = template(context);
-        return html
-    })
-    container.append(videos)
-      $(".video-table").on("durationchange", function() {
-      var tablePlayer = videoHandler('.table-player-container');
-    })
-    
+function generateGridVideos(data) {
+  var container = $("#gridContainer")
+
+  container.empty();
+  if (data.responseVideos.length === 0) {
+    var content = document.getElementById("no-results-table-template")
+    if (!content) return
+    var source = content.innerHTML;
+    var template = Handlebars.compile(source);
+    var context = {};
+    var html = template(context);
+    container.append(html)
+    return
+  }
+  const videos = data.responseVideos.map(function (video) {
+    video.videoUrl = generateVideoUrl(video.localId)
+    video.tags = Array.isArray(video.tags.item) ? video.tags.item : [video.tags.item];
+    var content = document.getElementById("video-table-template")
+    if (!content) return
+    var source = content.innerHTML;
+    var template = Handlebars.compile(source);
+    var context = video;
+    var html = template(context);
+    console.log(video);
+    return html;
+  })
+  container.append(videos)
+  $(".video-table").on("durationchange", function () {
+    var tablePlayer = videoHandler('.table-player-container');
+  })
+
 }
 
 function searchVideos(start, videoText, path) {
-	currentSearchVale = videoText
-	var api = "/api/1/services/search.json?start="+start+"&searchValue="+videoText.toLowerCase()+"&path="+path
-    $.get(api)
-      .done(function(data) {
-           if(data) {
-            var p = new Promise(function(resolve){
-              generateGridVideos(data)        
-              resolve('success')
-           })
-           p.then(function() {
-               $("#gridContainer").css("display","block");
-                generatePagination((data.totalCount/10) + 1, data.selectedPage)
-                handlePagination()
-                $('#input-search-text').val(currentSearchVale)
-           })
-          }   
+  currentSearchVale = videoText
+  var api = "/api/1/services/search.json?start=" + start + "&searchValue=" + videoText.toLowerCase() + "&path=" + path
+  $.get(api)
+    .done(function (data) {
+      if (data) {
+        var p = new Promise(function (resolve) {
+          generateGridVideos(data)
+          resolve('success')
         })
-      .fail(function(error) {
-          console.log(error)   
-        });
+        p.then(function () {
+          $("#gridContainer").css("display", "block");
+          generatePagination((data.totalCount / 10) + 1, data.selectedPage)
+          handlePagination()
+          $('#input-search-text').val(currentSearchVale)
+        })
+      }
+    })
+    .fail(function (error) {
+      console.log(error)
+    });
 }
 
 function requestVideos(start, categoryPath) {
-    $.get("/api/1/services/videos.json?start="+start+"&category="+categoryPath+"&searchInput="+currentSearchVale)
-      .done(function(data) {
-           if(data) {
-            var p = new Promise(function(resolve) {
-              generateGridVideos(data)        
-              resolve('success')
-           })
-           p.then(function(){
-               $("#gridContainer").css("display","block");
-                   generatePagination((data.totalCount/10) + 1, data.selectedPage)
-    			   handlePagination()
-           })
-          }   
+  $.get("/api/1/services/videos.json?start=" + start + "&category=" + categoryPath + "&searchInput=" + currentSearchVale)
+    .done(function (data) {
+      if (data) {
+        var p = new Promise(function (resolve) {
+          generateGridVideos(data)
+          resolve('success')
         })
-      .fail(function(error) {
-          console.log(error)   
-        });
+        p.then(function () {
+          $("#gridContainer").css("display", "block");
+          generatePagination((data.totalCount / 10) + 1, data.selectedPage)
+          handlePagination()
+        })
+      }
+    })
+    .fail(function (error) {
+      console.log(error)
+    });
 }
 
-function categoryRedirect(tagName){
-	console.log(tagName)
-    currentSearchVale = tagName
-    localStorage.removeItem('jstree');
-    localStorage.setItem('tagName', tagName)
-    window.location.replace("/categories");
-    
+function categoryRedirect(tagName) {
+  console.log(tagName)
+  currentSearchVale = tagName
+  localStorage.removeItem('jstree');
+  localStorage.setItem('tagName', tagName)
+  window.location.replace("/categories");
+
 }
 
 

a57b28fd0630490b4830a439c65b78ce458957f0
jvega190 <jose.vega@craftersoftware.com>
2019-10-11 10:48:54 -0600

    Added postfix - featured field

--- scripts/pages/page-home.groovy
+++ scripts/pages/page-home.groovy
@@ -1,6 +1,6 @@
 import scripts.utils.CarouselHelper
 
 def carouselHelper = new CarouselHelper(elasticsearch, urlTransformationService);
-def carouselVideosStatement = "content-type:\"/page/page-video\" AND featured: \"true\""
+def carouselVideosStatement = "content-type:\"/page/page-video\" AND featured_b: \"true\""
 
 templateModel.featuredVideos = carouselHelper.getCarouselList(carouselVideosStatement)
